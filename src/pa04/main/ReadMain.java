d71f611cd5f3922aca811ef6862acd156a7f1507
josei <josei@DESKTOP-48TQE4N>
2021-01-29 19:42:09 +0000

    pa04: Deserializacion Json v1

--- src/pa04/main/ReadMain.java
+++ src/pa04/main/ReadMain.java
@@ -3,11 +3,22 @@ package pa04.main;
 import java.io.File;
 import java.io.IOException;
 import pa04.model.Person;
+import pa04.persistence.PersonJsonFileReader;
 import pa04.persistence.PersonNativeFileReader;
-public class ReadMain{
+
+public class ReadMain {
     
-    public static void main(String[] args) throws IOException, ClassNotFoundException{
-        Person person= new PersonNativeFileReader(new File("maria.person")).read();
-        System.out.println(person.getName() + " nació el " + person.getBirthdate());
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Person person = 
+                new PersonNativeFileReader(new File("Maria.person")).read();
+        System.out.println("Leyendo fichero binario");
+        System.out.println(person.getName() + " nacida el " 
+                + person.getBirthdate());
+        
+        Person person1 = 
+                new PersonJsonFileReader(new File("Maria.json")).read();
+        System.out.println("Leyendo fichero json");
+        System.out.println(person1.getName() + " nacida el " 
+                + person1.getBirthdate());        
     }
 }
